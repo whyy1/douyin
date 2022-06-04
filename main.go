6e3e2880a9cbc2bb6bbccd4afa456b8b7f95565e
whyy1 <1006307055@qq.com>
2022-06-04 23:21:42 +0800

    dao层user实现

--- main.go
+++ main.go
@@ -1,20 +1,16 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
+
+var db gorm.DB
 
 func main() {
 	// 1.创建路由
 	r := gin.Default()
-	// 2.绑定路由规则，执行的函数
-	// gin.Context，封装了request和response
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "hello World!")
-	})
-	// 3.监听端口，默认在8080
-	// Run("里面不指定端口号默认为8080")
+	initRouter(r)
+	InitDB(db)
 	r.Run(":8080")
 }
